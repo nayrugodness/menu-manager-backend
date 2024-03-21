32129141eb6e61b4b437ef95fd176e51398aa6f3
nayrugodness <nanualexandraramirez@gmail.com>
2024-03-21 15:10:11 -0500

    adding models, platillo and categoria platillo and their attributes

--- menu/menu_manager/models.py
+++ menu/menu_manager/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class CategoriaPlatillo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Platillo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    precio = models.IntegerField()
+    imagen = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey(CategoriaPlatillo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
