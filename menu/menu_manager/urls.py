419e978c2282e7964419a15f4e808b0e397a14a8
nayrugodness <nanualexandraramirez@gmail.com>
2024-03-21 15:27:06 -0500

    adding app urls

---
+++ menu/menu_manager/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoriaPlatilloViewSet, PlatilloViewSet
+
+# Create a router and register our viewsets with it
+router = DefaultRouter()
+router.register(r'categorias', CategoriaPlatilloViewSet)
+router.register(r'platillos', PlatilloViewSet)
+
+# The API URLs are now determined automatically by the router
+urlpatterns = [
+    path('', include(router.urls)),
+]
