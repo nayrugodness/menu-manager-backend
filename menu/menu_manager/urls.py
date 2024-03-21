d2eb074af815409d7dcc97ec7893f8e57d99cfef
nayrugodness <nanualexandraramirez@gmail.com>
2024-03-21 15:49:56 -0500

    serializers and urls fixed, the api is exposed

--- menu/menu_manager/urls.py
+++ menu/menu_manager/urls.py
@@ -4,8 +4,8 @@ from .views import CategoriaPlatilloViewSet, PlatilloViewSet
 
 # Create a router and register our viewsets with it
 router = DefaultRouter()
-router.register(r'categorias', CategoriaPlatilloViewSet)
-router.register(r'platillos', PlatilloViewSet)
+router.register('categorias', CategoriaPlatilloViewSet)
+router.register('platillos', PlatilloViewSet)
 
 # The API URLs are now determined automatically by the router
 urlpatterns = [
