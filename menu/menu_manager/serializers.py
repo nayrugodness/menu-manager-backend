5ecf21ffc146d71fad51ab8d9c458b999d0036e7
nayrugodness <nanualexandraramirez@gmail.com>
2024-03-21 15:34:11 -0500

    fixing serializers

--- menu/menu_manager/serializers.py
+++ menu/menu_manager/serializers.py
@@ -1,11 +1,14 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import CategoriaPlatilloViewSet, PlatilloViewSet
+from rest_framework import serializers
+from .models import CategoriaPlatillo, Platillo
 
-router = DefaultRouter()
-router.register(r'categorias', CategoriaPlatilloViewSet)
-router.register(r'platillos', PlatilloViewSet)
+class CategoriaPlatilloSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CategoriaPlatillo
+        fields = ('id', 'nombre')
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
+class PlatilloSerializer(serializers.ModelSerializer):
+    categoria_nombre = serializers.CharField(source='categoria.nombre', read_only=True)
+
+    class Meta:
+        model = Platillo
+        fields = ('id', 'nombre', 'precio', 'imagen', 'descripcion', 'categoria', 'categoria_nombre')
