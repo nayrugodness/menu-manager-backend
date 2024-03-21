a931e2787cccd9e21671a4c707cc20df28f17fc0
nayrugodness <nanualexandraramirez@gmail.com>
2024-03-21 15:22:38 -0500

    adding models inside admin

--- menu/menu_manager/admin.py
+++ menu/menu_manager/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CategoriaPlatillo, Platillo
 
-# Register your models here.
+@admin.register(CategoriaPlatillo)
+class CategoriaPlatilloAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+
+@admin.register(Platillo)
+class PlatilloAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'precio', 'categoria')
+    list_filter = ('categoria',)  # Add filter by category
+    search_fields = ('nombre', 'descripcion')  # Add search by name and description
